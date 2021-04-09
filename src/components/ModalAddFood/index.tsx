3e31061c0f13aad547f818e50e536d2ab195e75e
Allisson Santos <allisson.ferreira@outlook.com>
2021-04-09 19:25:48 -0300

    Migrate ModalAddFood to TS

---
+++ src/components/ModalAddFood/index.tsx
@@ -0,0 +1,45 @@
+import { useRef } from 'react';
+import { FiCheckSquare } from 'react-icons/fi';
+
+import { Form } from './styles';
+import Modal from '../Modal';
+import Input from '../Input';
+
+import FoodInterface from '../../interfaces/FoodInterface';
+
+interface ModalEditFoodProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  handleAddFood: (food:FoodInterface) => void;
+}
+
+function ModalAddFood(props:ModalEditFoodProps) {
+  const { isOpen, setIsOpen, handleAddFood } = props;
+
+  const handleSubmit = async (data:FoodInterface) => {
+    handleAddFood(data);
+    setIsOpen();
+  };
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form onSubmit={handleSubmit}>
+        <h1>Novo Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+        <button type="submit" data-testid="add-food-button">
+          <p className="text">Adicionar Prato</p>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+}
+
+export default ModalAddFood;
